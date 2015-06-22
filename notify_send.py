83c7738fe4896e27dcdcd6426989d2c9de762303
Petr Zemek <s3rvac@gmail.com>
2015-06-22 19:42:38 +0200

    Bump the script version to '0.2 (dev)'.

--- notify_send.py
+++ notify_send.py
@@ -47,7 +47,7 @@ SCRIPT_NAME = 'notify_send'
 SCRIPT_AUTHOR = 's3rvac'
 
 # Version of the script.
-SCRIPT_VERSION = '0.1'
+SCRIPT_VERSION = '0.2 (dev)'
 
 # License under which the script is distributed.
 SCRIPT_LICENSE = 'MIT'
