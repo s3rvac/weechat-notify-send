ecf083c9bc2b4e8b4b844b0fc49c0eff5c63e792
Petr Zemek <s3rvac@gmail.com>
2015-06-23 20:03:24 +0200

    Add support for tests (nose) and linting (flake8).

---
+++ Makefile
@@ -0,0 +1,36 @@
+#
+# Project:   weechat-notify-send
+# Copyright: (c) 2015 by Petr Zemek <s3rvac@gmail.com> and contributors
+# License:   MIT, see the LICENSE file for more details
+#
+# A GNU Makefile for the project.
+#
+
+.PHONY: help clean lint tests tests-coverage
+
+help:
+	@echo "Use \`make <target>', where <target> is one of the following:"
+	@echo "  clean          - remove all generated files"
+	@echo "  lint           - check code style with flake8"
+	@echo "  tests          - run tests"
+	@echo "  tests-coverage - obtain test coverage"
+
+clean:
+	@find . -name '__pycache__' -exec rm -rf {} +
+	@find . -name '*.py[co]' -exec rm -f {} +
+	@rm -rf .coverage coverage
+	@rm -rf .tox retdec_python.egg-info
+
+lint:
+	@flake8 --ignore=E402,E501 notify_send.py notify_send_tests.py
+
+tests:
+	@nosetests notify_send_tests.py
+
+tests-coverage:
+	@nosetests notify_send_tests.py \
+		--with-coverage \
+		--cover-package notify_send \
+		--cover-erase \
+		--cover-html \
+		--cover-html-dir coverage
---
+++ notify_send_tests.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# Project:     weechat-notify-send
+# Homepage:    https://github.com/s3rvac/weechat-notify-send
+# Description: Unit tests for the project.
+# License:     MIT (see below)
+#
+# Copyright (c) 2015 by Petr Zemek <s3rvac@gmail.com> and contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+import sys
+import unittest
+
+try:
+    from unittest import mock  # Python 3
+except ImportError:
+    import mock  # Python 2
+
+# We need to mock the 'weechat' import because the tests do not run under
+# weechat. Moreover, it allows us to test the module properly.
+weechat = mock.Mock()
+sys.modules['weechat'] = weechat
