5e58dd66c7e60b8d4443e36e11f07f5c88c03c5a
Petr Zemek <s3rvac@gmail.com>
2020-06-20 14:19:06 +0200

    Bump the version number to "0.10 (dev)".

--- notify_send.py
+++ notify_send.py
@@ -50,7 +50,7 @@ SCRIPT_NAME = 'notify_send'
 SCRIPT_AUTHOR = 's3rvac'
 
 # Version of the script.
-SCRIPT_VERSION = '0.9'
+SCRIPT_VERSION = '0.10 (dev)'
 
 # License under which the script is distributed.
 SCRIPT_LICENSE = 'MIT'
